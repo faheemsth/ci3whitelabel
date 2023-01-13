3854d8fecb8e6e276042139bd6702f3f0c8daad7
wikkhaira <waqassth23@gmail.com>
2023-01-13 12:40:22 +0500

    2nd Commit

--- mvc/logs/log-2023-01-13.php
+++ mvc/logs/log-2023-01-13.php
@@ -73,3 +73,172 @@ ERROR - 2023-01-13 11:38:41 --> Could not find the language line "menu_Workshop 
 ERROR - 2023-01-13 11:38:42 --> 404 Page Not Found: Assets/media
 ERROR - 2023-01-13 11:38:42 --> 404 Page Not Found: Assets/media
 ERROR - 2023-01-13 11:38:42 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:51:24 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 11:51:24 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 11:51:24 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 11:51:24 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 11:51:24 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 11:51:24 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 11:51:24 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 11:51:25 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:51:25 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:51:26 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:51:49 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 11:51:49 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 11:51:49 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 11:51:49 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 11:51:49 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 11:51:49 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 11:51:49 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 11:51:49 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:51:49 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:51:51 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:52:20 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 11:52:20 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 11:52:20 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 11:52:20 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 11:52:20 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 11:52:20 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 11:52:20 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 11:52:20 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:52:20 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:52:22 --> 404 Page Not Found: Assets/media
+ERROR - 2023-01-13 11:52:31 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 11:52:31 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 11:52:31 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 11:52:31 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 11:52:31 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 11:52:31 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 11:52:31 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 11:52:31 --> Severity: error --> Exception: syntax error, unexpected ',', expecting identifier (T_STRING) or variable (T_VARIABLE) or '{' or '$' C:\xampp\htdocs\portal\mvc\views\regtype\view.php 6
+ERROR - 2023-01-13 11:52:33 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 11:52:33 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 11:52:33 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 11:52:33 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 11:52:33 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 11:52:33 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 11:52:33 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 11:52:38 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 11:52:38 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 11:52:38 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 11:52:38 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 11:52:38 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 11:52:38 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 11:52:38 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:01:05 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:01:05 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:01:05 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:01:05 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:01:05 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:01:05 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:01:05 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:06:40 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:06:40 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:06:40 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:06:40 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:06:40 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:06:40 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:06:40 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:06:40 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:06:40 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:06:41 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:06:41 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:06:41 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:07:05 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:07:05 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:07:05 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:07:05 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:07:05 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:07:05 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:07:05 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:07:05 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:07:05 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:07:06 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:07:06 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:07:08 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:07:32 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:07:32 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:07:32 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:07:32 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:07:32 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:07:32 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:07:32 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:07:32 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:07:32 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:07:33 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:07:33 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:07:35 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:11:50 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:11:50 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:11:50 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:11:50 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:11:50 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:11:50 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:11:50 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:11:50 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:11:50 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:11:50 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:11:51 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:11:53 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:12:37 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:12:37 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:12:37 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:12:37 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:12:37 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:12:37 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:12:37 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:12:37 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:12:37 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:12:37 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:12:37 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:12:40 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:13:49 --> 404 Page Not Found: Assets/frontend
+ERROR - 2023-01-13 12:13:52 --> 404 Page Not Found: Assets/frontend
+ERROR - 2023-01-13 12:15:27 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:15:27 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:15:27 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:15:27 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:15:27 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:15:27 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:15:27 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:15:27 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:15:27 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:15:27 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:15:27 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:15:30 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:15:49 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:15:49 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:15:49 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:15:49 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:15:49 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:15:49 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:15:49 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:15:49 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:15:49 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:15:50 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:15:50 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:15:52 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:16:39 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:16:39 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:16:39 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:16:39 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:16:39 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:16:39 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:16:39 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:16:39 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:16:39 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:16:40 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:16:40 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:16:41 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:24:04 --> Could not find the language line "menu_Registrations"
+ERROR - 2023-01-13 12:24:04 --> Could not find the language line "menu_Registration Type"
+ERROR - 2023-01-13 12:24:04 --> Could not find the language line "menu_Conference"
+ERROR - 2023-01-13 12:24:04 --> Could not find the language line "menu_Abstract"
+ERROR - 2023-01-13 12:24:04 --> Could not find the language line "menu_cabstract"
+ERROR - 2023-01-13 12:24:04 --> Could not find the language line "menu_Workshop"
+ERROR - 2023-01-13 12:24:04 --> Could not find the language line "menu_Workshop Type"
+ERROR - 2023-01-13 12:24:04 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:24:04 --> Could not find the language line "menu_regtype"
+ERROR - 2023-01-13 12:24:05 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:24:05 --> 404 Page Not Found: Regtype/assets
+ERROR - 2023-01-13 12:24:07 --> 404 Page Not Found: Regtype/assets
