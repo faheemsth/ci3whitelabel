683f7f7682153ff6c2a4f2363ce48d11e080fba5
wikkhaira <waqassth23@gmail.com>
2023-01-16 10:47:05 +0500

    3rd Commit

--- mvc/views/regtype/edit.php
+++ mvc/views/regtype/edit.php
@@ -1,24 +1,24 @@
 
-<div class="box">
-    <div class="box-header">
-        <h3 class="box-title"><i class="fa fa-calendar"></i> <?=$this->lang->line('panel_title')?></h3>
+<div class=" card">
+    <div class=" card-header">
+        <h3 class="card-title"><i class="fa fa-calendar mx-2"></i> <?=$this->lang->line('panel_title')?></h3>
         <ol class="breadcrumb">
             <li><a href="<?=base_url("dashboard/index")?>"><i class="fa fa-laptop"></i> <?=$this->lang->line('menu_dashboard')?></a></li>
             <li><a href="<?=base_url("regtype/index")?>"><?=$this->lang->line('menu_regtype')?></a></li>
-            <li class="active"><?=$this->lang->line('menu_edit')?> <?=$this->lang->line('menu_regtype')?></li>
+            <li class="active mx-2"><?=$this->lang->line('menu_edit')?> <?=$this->lang->line('menu_regtype')?></li>
         </ol>
-    </div><!-- /.box-header -->
+    </div><!-- /. card-header -->
     <!-- form start -->
-    <div class="box-body">
+    <div class=" card-body">
         <div class="row">
             <div class="col-sm-10">
                 <form class="form-horizontal" role="form" method="post">
-                    <div class="form-group <?=form_error('title') ? 'has-error' : '' ?>" >
+                    <div class="form-group mb-7 <?=form_error('title') ? 'has-error' : '' ?>" >
                         <label for="title" class="col-sm-2 control-label">
                             <?=$this->lang->line("regtype_title")?> <span class="text-red">*</span>
                         </label>
                         <div class="col-sm-6">
-                            <input type="text" class="form-control" id="title" name="title" value="<?=set_value('title', $regtype->title)?>">
+                            <input type="text" class="form-control bg-transparent" id="title" name="title" value="I am the title">
                         </div>
                         <span class="col-sm-4 control-label">
                             <?php echo form_error('title'); ?>
@@ -26,7 +26,7 @@
                     </div>
 
 
-                    <div class="form-group">
+                    <div class="form-group mb-7">
                         <div class="col-sm-offset-2 col-sm-8">
                             <input type="submit" class="btn btn-success" value="<?=$this->lang->line("update_regtype")?>" >
                         </div>
